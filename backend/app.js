916169c433a6225617cccb618c1b5f1317796660
tbensheimer <tbensheimer@gmail.com>
2023-03-26 01:48:50 -0400

    implement userRoutes into app

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
@@ -8,10 +9,12 @@ app.use(express.json());
 
 mongoose.set({strictQuery: true});
 
-mongoose.connect(process.env.DB)
+mongoose.connect(process.env.DB)  //get real database created and connected and test in postman
 .then(() => {
     app.listen(process.env.PORT)
 })
 .catch((error) => {
     console.log(error);
 })
+
+app.use('/user', userRoutes);
