7c8ed70bd535fbb21308598adb0c536e6caf5bab
tbensheimer <tbensheimer@gmail.com>
2023-03-31 16:10:15 -0400

    Login page and useLogin hook

---
+++ src/hooks/useLogin.js
@@ -0,0 +1,37 @@
+import {useState} from "react";
+import {useDispatch} from "react-redux";
+import {login} from "../redux/store";
+
+export default function useLogin() {
+const [loading, setLoading] = useState(false);
+const [error, setError] = useState(null);
+const dispatch = useDispatch();
+
+const loginUser = async(email, password) => {
+    setLoading(true);
+    setError(null);
+   
+    const response = await fetch("/user/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({email, password})
+    });
+
+    const data = await response.json();
+
+    if(!response.ok) {
+        setError(data.error)
+        setLoading(false);
+    }
+
+    if(response.ok) {
+        setLoading(false);
+        localStorage.setItem('user', data);
+        dispatch(login(data));   
+        console.log("successfully logged in");
+    }
+}
+return {error, loading, loginUser}
+}
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,8 +1,35 @@
 import "./Login.css"
+import {useState} from "react";
+import useLogin from "../../hooks/useLogin";
 
-export default function Login() {
+export default function Signup() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const {loading, error, loginUser} = useLogin();
+
+    const handleLoginSubmit = (e) => {
+        e.preventDefault();
+
+        loginUser(email, password);
+    }
 
     return (
-        <div>Login</div>
+        <form className="auth-form" onSubmit={handleLoginSubmit} >
+            <h2>Log In</h2>
+
+            <label>
+                <span>Email:</span>
+                <input required type="email" onChange={e => setEmail(e.target.value)} value={email} />
+            </label>
+
+            <label>
+                <span>Password:</span>
+                <input required type="password" onChange={e => setPassword(e.target.value)} value={password} />
+            </label>
+
+            {!loading && <button className="btn">Login</button>}
+            {loading && <button disabled type="button" className="btn">Loading...</button>}
+            {error && <div className="error">{error}</div>}
+            </form>
     )
 }
