37f2349ff22a813b413df6e9f43704f6cc16a2ff
tbensheimer <tbensheimer@gmail.com>
2023-04-09 00:33:01 -0400

    Project details page set up

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,8 +1,21 @@
 import "./Project.css"
+import { useParams } from "react-router"
 
 export default function Project() {
+    const id = useParams();
+  //  const {project, error} = useFetchProjecthook();
+
+    if(error) {
+        return <div className="error">{error}</div>
+    }
+
+    if(!project) {
+        return <div className="loading">Loading...</div>
+    }
 
     return (
-        <div>Project</div>
+        <div className="project-details">
+            <h1>{project.name}</h1>
+        </div>
     )
 }
