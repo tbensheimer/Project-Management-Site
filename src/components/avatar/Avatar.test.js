efb5f7d14b4a384d7f2eb8286462d425ee381e57
tbensheimer <tbensheimer@gmail.com>
2023-04-30 16:06:58 -0400

    Avatar component testing

--- src/components/avatar/Avatar.test.js
+++ src/components/avatar/Avatar.test.js
@@ -1,11 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom'
 import Avatar from "./Avatar"
 
 it('Should render avatar component with correct src', () => {
-    render(<Avatar src={'userImage.png'}/>)
+    render(<Avatar src={"userImage.png"}/>)
 
+    const rootURL = window.location.href;
     const avatar = screen.getByRole('img');
 
     expect(avatar).toBeInTheDocument();
-    expect(avatar.src).toBe('userImage.png');
+    expect(avatar.src).toBe(`${rootURL}userImage.png`);
+    expect(avatar.alt).toBe('user avatar');
+    expect(avatar.parentNode.className).toBe('avatar');
 })
